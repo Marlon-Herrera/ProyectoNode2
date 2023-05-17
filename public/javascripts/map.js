04af6c3107fa35e9a9fbb040f6fdf46aaa97fd73
Marlon H <marlon7hrr@gmail.com>
2023-05-17 00:17:35 -0500

    Se realizaron las vistas html de las bicicletas utilizando los metodos de transferencia de datos para su funcionamiento

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,6 +4,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy: <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([4.5235149,-74.1222651]).addTo(map);
-L.marker([4.5794549,-74.1600856]).addTo(map);
-L.marker([4.5844364,-74.1790023]).addTo(map);
+L.marker([4.5235149,-74.1222651]).addTo(map); //aurora
+L.marker([4.5794549,-74.1600856]).addTo(map); //ud
+L.marker([4.5844364,-74.1790023]).addTo(map); //perdomo
