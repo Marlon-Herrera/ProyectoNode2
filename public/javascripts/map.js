f66465a22b2996c3ebd7dfe8594fdc9ef4cbaa44
Marlon H <marlon7hrr@gmail.com>
2023-05-17 01:11:06 -0500

    Se implemento el llamado al complemento de javascript, ajax para la ilustración grafica asimismo los metodos de intercambio de informacion para las demostraciones graficas en el mapa

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,6 +4,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy: <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([4.5235149,-74.1222651]).addTo(map); //aurora
-L.marker([4.5794549,-74.1600856]).addTo(map); //ud
-L.marker([4.5844364,-74.1790023]).addTo(map); //perdomo
+// L.marker([4.5235149,-74.1222651]).addTo(map); //aurora
+// L.marker([4.5794549,-74.1600856]).addTo(map); //ud
+// L.marker([4.5844364,-74.1790023]).addTo(map); //perdomo
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
