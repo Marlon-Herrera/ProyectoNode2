cf3680bf82a5b228f1c7efd3c1972f2d2225ada1
Marlon H <marlon7hrr@gmail.com>
2023-05-30 10:56:15 -0500

    Se agrego el tercer describe jasmine que corresponde a la prueba de buscar una bicicleta

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -1,30 +1,30 @@
-var Bicicleta = function(id, color, modelo, ubicacion){
+var Bicicleta = function (id, color, modelo, ubicacion) {
     this.id = id;
     this.color = color;
     this.modelo = modelo;
     this.ubicacion = ubicacion;
 }
 
-Bicicleta.prototype.toString = function(){
+Bicicleta.prototype.toString = function () {
     return 'id: ' + this.id + " | color: " + this.color;
 }
 
 Bicicleta.allBicis = [];
-Bicicleta.add = function(aBici){
+Bicicleta.add = function (aBici) {
     Bicicleta.allBicis.push(aBici);
 }
 
-Bicicleta.findById = function(aBiciId){
+Bicicleta.findById = function (aBiciId) {
     var aBici = Bicicleta.allBicis.find(x => x.id == aBiciId);
     if (aBici)
         return aBici;
-    else 
+    else
         throw new Error(`No existe una bicicleta con el id ${aBiciId}`);
 }
 
-Bicicleta.removeBtId = function(aBiciId){
-    for(var i = 0; i < Bicicleta.allBicis.length; i++){
-        if(Bicicleta.allBicis[i].id == aBiciId){
+Bicicleta.removeById = function (aBiciId) {
+    for (var i = 0; i < Bicicleta.allBicis.length; i++) {
+        if (Bicicleta.allBicis[i].id == aBiciId) {
             Bicicleta.allBicis.splice(i, 1);
             break;
         }
--- spec/models/bicicleta_test.spec.js
+++ spec/models/bicicleta_test.spec.js
@@ -1,5 +1,9 @@
 var Bicicleta = require('../../models/bicicleta');
 
+beforeEach(() => {
+    Bicicleta.allBicis = [];
+});
+
 describe('Bicicleta.allBicis', () => {
     it('comienza vacia', () => {
         expect(Bicicleta.allBicis.length).toBe(0);
@@ -9,9 +13,25 @@ describe('Bicicleta.allBicis', () => {
 describe('Bicicleta.add', () => {
     it('agregamos una', () => {
         expect(Bicicleta.allBicis.length).toBe(0)
-        var a = new Bicicleta(1, 'rojo', 'urbana', [4.5235149,-74.1222651]);
+        var a = new Bicicleta(1, 'rojo', 'urbana', [4.5235149, -74.1222651]);
         Bicicleta.add(a);
         expect(Bicicleta.allBicis.length).toBe(1);
         expect(Bicicleta.allBicis[0]).toBe(a);
     });
 });
+
+describe('Bicicleta.findById', () => {
+    it('debe devolver la bici con id 1', () => {
+        expect(Bicicleta.allBicis.length).toBe(0);
+        var aBici = new Bicicleta(1, "verde", "urbana");
+        var aBici2 = new Bicicleta(2, "rojo", "montaña");
+        Bicicleta.add(aBici);
+        Bicicleta.add(aBici2);
+
+        var targetBici = Bicicleta.findById(1);
+        expect(targetBici.id).toBe(1);
+        expect(targetBici.color).toBe(aBici.color);
+        expect(targetBici.modelo).toBe(aBici.modelo);
+    });
+});
+
